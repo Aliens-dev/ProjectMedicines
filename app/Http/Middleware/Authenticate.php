a5ee38543cec1b4dd4a171936337ce0706544ac7
aliensDev <merazganabil34@gmail.com>
2021-08-25 12:51:12 +0100

    remove logs in middlewars

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,6 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            dd('here');
             return route('login');
         }
     }
