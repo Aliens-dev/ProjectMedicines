ee26442341ea4bda164e295e2b25f21e14e7d052
aliensDev <merazganabil34@gmail.com>
2021-08-28 23:26:25 +0100

    add db

--- tests/Feature/Deseaes/CreateDeseaseTest.php
+++ tests/Feature/Deseaes/CreateDeseaseTest.php
@@ -4,7 +4,15 @@
 namespace Deseaes;
 
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 class CreateDeseaseTest extends \Tests\TestCase
 {
+    use RefreshDatabase;
 
+    /** @test */
+    public function testexample()
+    {
+        $this->assertTrue(true);
+    }
 }
